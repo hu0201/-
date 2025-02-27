0dcd241bd51938dd637b06d7f6398291b7c4ff19
hu0201 <easyhu88@gmail.com>
2025-02-27 14:20:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,38 +3,51 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
-
+from openai import OpenAI
 app = Flask(__name__)
 
-# 从环境变量中获取 Line Bot 的信息
+# 設定 LINE Channel Access Token 和 Secret
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+api = os.getenv("OPENAI_API_KEY")
 
-# 如果没有环境变量，抛出异常
-if not LINE_CHANNEL_ACCESS_TOKEN or not LINE_CHANNEL_SECRET:
-    raise ValueError("LINE_CHANNEL_ACCESS_TOKEN or LINE_CHANNEL_SECRET is missing!")
-
+# Initialize OpenAI client
+client = OpenAI(api_key=api)
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-lhandler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-@app.route("/", methods=["GET"])
+line_handler = WebhookHandler(LINE_CHANNEL_SECRET)
+@app.route('/')
 def home():
-    return "Line Bot is running on Vercel!"
+    return "LINE BOT 首頁"
 
-# @app.route("/callback", methods=["POST"])
-# def callback():
-#     signature = request.headers.get("X-Line-Signature")
-#     body = request.get_data(as_text=True)
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
 
-#     try:
-#         lhandler.handle(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-#     return "OK"
+    return "OK"
 
-# @lhandler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     user_message = event.message.text
-#     reply_message = f"你剛剛說: {user_message}"
-#     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    
+    user_message = event.message.text
+    
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f"{user_message}"}
+    ]
+)
+    reply_message = completion.choices[0].message.content
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_message)
+    )
+
+if __name__ == "__main__":
+    app.run(port=8000)
